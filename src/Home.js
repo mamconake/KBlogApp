8af82b6553e6cb5c2a114da02af4bb71b52ec586
mamconake <naftalikipkemboi@gmail.com>
2023-03-13 17:38:30 +0300

    listing blogs using useState and map function

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,19 @@
 import { useState } from "react";
 const Home = () => {
-    //let name = "Dev";
-    const [name, setName] = useState('Dev');
-    const [age, setAge] = useState(20);
-    const handleClick = () =>{
-        setName('luigi');
-        setAge(33);
-    }
+    const[blogs, setBlogs] = useState([
+        {title: 'My new website', body:'I got you', author:'Jeff', id:1},
+        {title: 'Welcome Party', body:'It is lit', author:'Mane', id:2},
+        {title: 'Top billionares in Africa', body:'I want to be rich', author:'Kim', id:3},
+    ])
    
     return ( 
         <div className="Home">
-            <p>{name} is {age} years old</p>
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((blog)=>(
+                    <div className="blog-preview" key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author}</p>
+                    </div>
+            ))}
         </div>
      );
 }
