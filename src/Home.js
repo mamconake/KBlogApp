6ebc6a38ea640d6648072d2103e31805a6849215
mamconake <naftalikipkemboi@gmail.com>
2023-03-13 18:28:23 +0300

    Adding delete function by using props functionality

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,20 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
     const[blogs, setBlogs] = useState([
-        {title: 'My new website', body:'I got you', author:'Jeff', id:1},
+        {title: 'My new website', body:'I got you', author:'Kim', id:1},
         {title: 'Welcome Party', body:'It is lit', author:'Mane', id:2},
         {title: 'Top billionares in Africa', body:'I want to be rich', author:'Kim', id:3},
-    ])
+    ]);
+    const handleDelete=(id)=>{
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs)
+    }
    
     return ( 
         <div className="Home">
-            {blogs.map((blog)=>(
-                    <div className="blog-preview" key={blog.id}>
-                        <h2>{blog.title}</h2>
-                        <p>Written by {blog.author}</p>
-                    </div>
-            ))}
+           <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete}/>
+           <BlogList blogs={blogs.filter((blog)=> blog.author === 'Kim')} title={"Kim's Blogs"}/>
         </div>
      );
 }
