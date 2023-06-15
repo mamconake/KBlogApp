13208cf83c248cd89d4cc1a41cf338b3ca61feb5
mamconake <naftalikipkemboi@gmail.com>
2023-06-15 06:17:48 +0300

    updating with a custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,8 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 const Home = () => {
-    const[blogs, setBlogs] = useState(null);
-    const[isLoading, setIsLoading] = useState(true);
-    const[error, setError] = useState(null);
-    const handleDelete=(id)=>{
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs)
-    }
-    useEffect(()=>{
-        setTimeout(()=>{
-            fetch(' http://localhost:8000/blogs')
-        .then(res=>{
-            if(!res.ok){
-                throw Error('could not fetch the data for that resource');
-            }
-            return res.json();
-        })
-        .then(data=>{
-            //console.log(data);
-            setBlogs(data);
-            setIsLoading(false);
-            setError(null);
-        })
-        .catch(err =>{
-            setIsLoading(false);
-            setError(err.message);
-        })
-        }, 1000);
-    }, []);
+    const {data: blogs, isLoading, error, handleDelete} = useFetch(' http://localhost:8000/blogs');
    
     return ( 
         <div className="Home">
