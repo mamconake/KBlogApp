527f5a3e2880fa32241c5011a8380c93dd40a205
mamconake <naftalikipkemboi@gmail.com>
2023-06-15 05:48:24 +0300

    updating

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-    const[blogs, setBlogs] = useState([
-        {title: 'My new website', body:'I got you', author:'Kim', id:1},
-        {title: 'Welcome Party', body:'It is lit', author:'Mane', id:2},
-        {title: 'Top billionares in Africa', body:'I want to be rich', author:'Kim', id:3},
-    ]);
+    const[blogs, setBlogs] = useState(null);
+    const[isLoading, setIsLoading] = useState(true);
+    const[error, setError] = useState(null);
     const handleDelete=(id)=>{
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs)
     }
+    useEffect(()=>{
+        setTimeout(()=>{
+            fetch(' http://localhost:8000/blogs')
+        .then(res=>{
+            if(!res.ok){
+                throw Error('could not fetch the data for that resource');
+            }
+            return res.json();
+        })
+        .then(data=>{
+            //console.log(data);
+            setBlogs(data);
+            setIsLoading(false);
+            setError(null);
+        })
+        .catch(err =>{
+            setIsLoading(false);
+            setError(err.message);
+        })
+        }, 1000);
+    }, []);
    
     return ( 
         <div className="Home">
-           <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete}/>
-           <BlogList blogs={blogs.filter((blog)=> blog.author === 'Kim')} title={"Kim's Blogs"}/>
+            {isLoading && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blogs && <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete}/>}
+            {/* <BlogList blogs={blogs.filter((blog)=> blog.author === 'Kim')} title={"Kim's Blogs"}/> */}
+         {/* {blogs &&  <button onClick={()=> setName('Korir')}>change name</button>} */}
         </div>
      );
 }
